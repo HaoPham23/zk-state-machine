5c50843ca9f85efaaf456b8ba321578fa85455bb
Hao Pham-Duc <hao.pham@skymavis.com>
2025-01-29 13:40:36 +0700

    complete setup

--- program/src/bin/test.rs
+++ program/src/bin/test.rs
@@ -2,7 +2,6 @@
 
 use kzg_rs::{FIAT_SHAMIR_PROTOCOL_DOMAIN};
 use kzg_rs::{trusted_setup::KzgSettings, KzgError};
-use ff::{Field, PrimeField};
 use sp1_bls12_381::{Scalar, G1Affine, G2Affine};
 
 fn commit(poly: Vec<Scalar>, kzg_settings: &KzgSettings) -> Result<G1Affine, KzgError> {
@@ -15,92 +14,104 @@ fn commit(poly: Vec<Scalar>, kzg_settings: &KzgSettings) -> Result<G1Affine, Kzg
     Ok(commitment)
 }
 
-fn compute_lagrange_basis(kzg_settings: &KzgSettings) -> Result<Vec<G1Affine>, KzgError> {
-    let g1 = kzg_settings.g1_points;
-    let domain = kzg_settings.roots_of_unity;
+fn compute_lagrange_basis(tau: Scalar, domain: Vec<Scalar>) -> Result<Vec<G1Affine>, KzgError> {
     let mut basis: Vec<G1Affine> = Vec::new();
+    let g1 = G1Affine::generator();
     for i in 0..domain.len() {
-        let mut coeff = G1Affine::identity();
+        let mut li = Scalar::one();
         for j in 0..domain.len() {
-            println!("{:?} {:?}", i, j);
             if i != j {
-                let numerator = G1Affine::from(g1[0] * domain[j].neg()); // -j*G1
-                let denominator = domain[i] - domain[j];
-                let denominator = denominator.invert().unwrap();
-                coeff = G1Affine::from(g1[1].add_affine(&numerator) * denominator);
+                li *= (tau - domain[j]) * (domain[i] - domain[j]).invert().unwrap();
             }
         }
-        basis.push(coeff);
+        basis.push(G1Affine::from(g1 * li));
     }
     Ok(basis)
-    
-}
-
-// #[derive(PrimeField)]
-// #[PrimeFieldModulus = "52435875175126190479447740508185965837690552500527637822603658699938581184513"]
-// #[PrimeFieldGenerator = "2"]
-// #[PrimeFieldReprEndianness = "little"]
-// struct Fr([u64; 4]);
-
-struct PublicParams {
-    pub p: Scalar,
-    pub r: Scalar,
-    pub tau: Scalar,
-    pub g: Scalar, // El Gamal generator
-    pub g1s: Vec<G1Affine>,
-    pub g2s: Vec<G2Affine>
 }
 
 const D: usize = 16;
 const P: Scalar = Scalar([0x6730d2a0f6b0f624, 0x64774b84f38512bf, 0x4b1ba7b6434bacd7, 0x1a0111ea397fe69a]);
 const R: Scalar = Scalar([0xFFFFFFFF00000001, 0x53BDA402FFFE5BFE, 0x3339D80809A1D805, 0x73EDA753299D7D48]);
+const TAU: Scalar = Scalar([0x5, 0x0, 0x0, 0x0]);
+
+#[derive(Debug)]
+struct PublicParams {
+    pub p: Scalar,
+    pub r: Scalar,
+    pub g: Scalar, // El Gamal generator
+    pub g1_points: Vec<G1Affine>,
+    pub g2_points: Vec<G2Affine>,
+    pub g1_lagrange_basis: Vec<G1Affine>,
+}
 
 impl PublicParams {
-    fn setup() {
-        let p = P;
-        let r = R;
-        let g = Field::ONE;
-        let tau = Scalar::from(2); // TODO: random
-        let g1 = G1Affine::identity();
-        let g2 = G2Affine::identity();
-        let mut G1s = Vec::new();
-        let mut G2s = Vec::new();
-        for i in 0..D {
-            tau_power = tau.pow(Scalar::from(i as u64).unwrap());
-            G1s.push(G1Affine::from(g1 * Scalar::from(i as u64).pow(i)));
-            G2s.push(G2Affine::from(g2 * Scalar::from(i as u64)));
-        }
+    fn new(p: Scalar, r: Scalar, g: Scalar, g1_points: Vec<G1Affine>, g2_points: Vec<G2Affine>, g1_lagrange_basis: Vec<G1Affine>) -> PublicParams {
         PublicParams {
             p,
             r,
             g,
-            G1s,
-            G2s
+            g1_points,
+            g2_points,
+            g1_lagrange_basis
+        }
+    }
+
+    fn setup() -> PublicParams {
+        let p = P;
+        let r = R;
+        let g = Scalar::from(2);
+        let g1 = G1Affine::generator();
+        let g2 = G2Affine::generator();
+        let mut g1_points = Vec::new();
+        let mut g2_points = Vec::new();
+        let mut tau_pow = Scalar::from(1);
+        for _ in 0..D {
+            tau_pow *= TAU;
+            g1_points.push(G1Affine::from(g1 * tau_pow));
+            g2_points.push(G2Affine::from(g2 * tau_pow));
+        }
+        let domain = (0..D).map(|i| Scalar::from(i as u64)).collect();
+        let g1_lagrange_basis = compute_lagrange_basis(TAU, domain).unwrap();
+        PublicParams {
+            p,
+            r,
+            g,
+            g1_points,
+            g2_points,
+            g1_lagrange_basis,
         }
     }
 }
 
-fn setup() -> Result<KzgSettings, KzgError> {
-    const DEG: usize = 16;
-    let tau = Scalar::from(2); // random tau
-    let mut domain: [Scalar; DEG] = [Scalar::from(0); DEG];
-    for i in 0..DEG {
-        domain[i] = Scalar::from(i as u64);
+#[derive(Debug)]
+struct KZG {
+    g1_points: Vec<G1Affine>,
+    g2_points: Vec<G2Affine>,
+    g1_lagrange_basis: Vec<G1Affine>
+}
+
+impl KZG {
+    fn new(g1_points: Vec<G1Affine>, g2_points: Vec<G2Affine>, g1_lagrange_basis: Vec<G1Affine>) -> KZG {
+        KZG {
+            g1_points,
+            g2_points,
+            g1_lagrange_basis
+        }
     }
-    let g1 = G1Affine::identity();
-    let mut g1_points = [g1; DEG as usize];
-    for i in 0..DEG {
-        g1_points[i] = G1Affine::from(g1 * domain[i]);
+
+    fn commit(&self, poly: Vec<Scalar>) -> Result<G1Affine, KzgError> {
+        let mut commitment = G1Affine::identity();
+        for i in 0..poly.len() {
+            let coeff = G1Affine::from(self.g1_lagrange_basis[i] * poly[i]);
+            commitment = commitment.add_affine(&coeff);
+        }
+        Ok(commitment)
     }
 }
 
 fn main() {
-    let kzg_settings = KzgSettings::load_trusted_setup_file().unwrap();
-    println!("{:?}", FIAT_SHAMIR_PROTOCOL_DOMAIN);
-
-    let base = sp1_bls12_381::Scalar::from(2);
-    let v = kzg_rs::kzg_proof::compute_powers(&base, 1);
-    println!("{:?}", v);
-    let basis = compute_lagrange_basis(&kzg_settings).unwrap();
-    println!("{:?}", commit_evaluation(v, basis, &kzg_settings).unwrap());
+    let pp = PublicParams::setup();
+    let kzg = KZG::new(pp.g1_points, pp.g2_points, pp.g1_lagrange_basis);
+    let v = vec![Scalar::one(); D];
+    println!("{:?}", kzg.commit(v).unwrap());
 }
